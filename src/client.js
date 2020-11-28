a3c6a4e50080fc70b3b170928c0d786cf5533df8
arfaghif <52845090+arfaghif@users.noreply.github.com>
2020-11-28 16:57:47 +0700

    add axios

--- src/client.js
+++ src/client.js
@@ -1,36 +1,40 @@
 import React from 'react'
+var axios = require('axios');
 
+var SoaMessage = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+<soapenv:Body>
+  <ns1:updateBalance xmlns:ns1="http://jaxwsservice.codejava.net/" xmlns:ns3="http://www.example.org/FactorySchema" xmlns:ns4="http://codejava.net">
+  <UpdateBalance>
+        </UpdateBalance>
+  </ns1:updateBalance>
+</soapenv:Body>
+</soapenv:Envelope>`;
 
-var SoaMessage = `<?xml version='1.0' encoding='UTF-8'?>
-<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/">
-    <S:Body>
-        <ns2:bonjour xmlns:ns2="http://codejava.net/">
-        </ns2:bonjour>
-    </S:Body>
-</S:Envelope>`
+// var url = "http://localhost:8001/MyApp2/ws/hello?wsdl";
 
-var url = "http://localhost:8000/MyApp2/ws/hello?wsdl";
+var config = {
+    method: 'post',
+    url: "http://localhost:8001/MyApp2/ws/hello?wsdl",
+    headers: { 
+    //   'Content-MD5': 'text/xml', 
+      'Content-Type': 'text/xml',
+    //   "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    },
+    data : SoaMessage
+  };
+// var url = "http://localhost:8001/MyApp2/ws/hello?wsdl";
 
 export default class Client{
     numbers(){
         // let result = new Promise(()=>{
-            var request = new XMLHttpRequest();
-            request.open("POST",url,true);
-            request.onreadystatechange = function(){
-                if (request.readyState===4){
-                    var res = request.responseXML;
-                    res = res.getElementsByTagName("return")[0].childNodes[0].data;
-                    console.log(res);
-                    return("xx");
-
-                }
-            }
-            request.setRequestHeader("Content-type","text/xml")
-            request.setRequestHeader("Access-Control-Allow-Origin" ,"http://localhost:3000");
-            request.setRequestHeader("Access-Control-Allow-Origin" ,"http://localhost:8000");
-            console.log("1");
-            request.send(SoaMessage);
-            
+            axios(config)
+            .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
             // var request = new XMLHttpRequest();
             // request.open("POST",url,true);
             // console.log("2");
